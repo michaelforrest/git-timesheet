ad3f4b1ab3194f478af7e28bea9c01b4cb086932
Michael Forrest <michael.forrest@gmail.com>
2012-02-06 10:59:32 +0000

    date format

--- git-timesheet.rb
+++ git-timesheet.rb
@@ -31,10 +31,10 @@ else
   log_lines = `git log --no-merges --simplify-merges --author="#{options[:author].gsub('"','\\"')}" --format="%ad %s <%h>" --date=iso --since="#{options[:since].gsub('"','\\"')}"`.split("\n")
   day_entries = log_lines.inject({}) {|days, line|
     timestamp = Time.parse line.slice!(0,25)
-    day = timestamp.strftime("%E %Y-%m-%d")
+    day = timestamp.strftime("%a %d %b %Y")
     days[day] ||= []
     days[day] << timestamp.strftime("%H:%S ") + line.strip
     days
   }.sort{|a,b| a[0]<=>b[0]}
-  puts day_entries.map{|day, entries| "#{day}\n#{'='*10}\n\n#{entries.sort.join("\n")}\n\n"}
+  puts day_entries.map{|day, entries| "#{day}\n#{'='*15}\n\n#{entries.sort.join("\n")}\n\n"}
 end
